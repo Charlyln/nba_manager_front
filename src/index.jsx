0271dd6dd581f54805d4e687f7722c6f5a1ece59
Charlyln <charleshenry.lenue>
2020-08-27 19:17:59 +0200

    change router

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from './components/Home';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/home" component={Home} />
+        {/* <Route exact path="/" component={App} /> */}
+        <Route exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
-   
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+)
