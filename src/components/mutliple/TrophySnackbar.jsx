ca73973a02510a21387dfae666938f70b8980f11
Charlyln <charleshenry.lenue>
2020-11-29 15:11:05 +0100

    create trophySnackbar with trophy logo

---
+++ src/components/mutliple/TrophySnackbar.jsx
@@ -0,0 +1,65 @@
+import { Fab, Grid, Snackbar, Typography } from '@material-ui/core'
+import React from 'react'
+import trophy from '../../trophy.png'
+import { Link } from 'react-router-dom'
+
+function TrophySnackbar({
+  openTrophySnackbar,
+  closeTrophySnackbar,
+  trophyName
+}) {
+  return (
+    <>
+      <Link to="/profil">
+        <Snackbar
+          open={openTrophySnackbar}
+          autoHideDuration={7000}
+          onClose={closeTrophySnackbar}
+          message="Note archived"
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        >
+          <Fab variant="extended" style={{ backgroundColor: '#4CAF50' }}>
+            <img
+              style={{
+                width: '35px',
+                height: '35px',
+                margin: '0px 10px 0px 0px'
+              }}
+              src={trophy}
+              alt="trophy"
+            />
+            <Grid container>
+              <Grid item>
+                <Typography
+                  variant="button"
+                  component="div"
+                  color="textSecondary"
+                  style={{
+                    margin: '-5px 0px 0px 0px',
+                    color: '#424242'
+                  }}
+                >
+                  <strong>Trophy</strong>
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography
+                  style={{
+                    margin: '15px 10px -1px -60px',
+                    color: '#424242'
+                  }}
+                  variant="caption"
+                  display="block"
+                  gutterBottom
+                >
+                  {trophyName}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Fab>
+        </Snackbar>
+      </Link>
+    </>
+  )
+}
+export default TrophySnackbar
