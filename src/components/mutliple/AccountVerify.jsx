4cca0f9f13463acd67b1cfe98ea7b182a20f0ab5
Charlyln <charleshenry.lenue>
2020-11-30 15:17:20 +0100

    add a request in account verify, to check if user exist

--- src/components/mutliple/AccountVerify.jsx
+++ src/components/mutliple/AccountVerify.jsx
@@ -1,14 +1,36 @@
-import React, { useState } from 'react'
-import { Redirect } from 'react-router-dom'
+import Axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Redirect, useHistory } from 'react-router-dom'
+import { apiUrl } from '../../apiUrl'
 
 function AccountVerify() {
-  const [uuid] = useState(window.localStorage.getItem('uuid'))
+  const history = useHistory()
+  const [UserUuid] = useState(window.localStorage.getItem('uuid'))
   const [SeasonUuid] = useState(window.localStorage.getItem('SeasonUuid'))
   const [TeamUuid] = useState(window.localStorage.getItem('TeamUuid'))
   const [canPlay] = useState(window.localStorage.getItem('canPlay'))
 
-  if (!uuid || !SeasonUuid || !TeamUuid || !canPlay) {
+  useEffect(() => {
+    AccountVerifyRequest()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (!UserUuid || !SeasonUuid || !TeamUuid || !canPlay) {
     return <Redirect to="/" />
+  }
+  const AccountVerifyRequest = async () => {
+    try {
+      const res = await Axios.get(`${apiUrl}/users/account/${UserUuid}`)
+      if (!res.data) {
+        window.localStorage.removeItem('uuid')
+        console.log('not found')
+        history.push('/')
+      } else {
+        console.log('found')
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return <></>
