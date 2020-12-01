abe70830af252423a14f170d2ebb52d1a4e99848
Charlyln <charleshenry.lenue>
2020-12-01 17:06:24 +0100

    add if in apiUrl to use the right adress

--- src/apiUrl.jsx
+++ src/apiUrl.jsx
@@ -1,2 +1,9 @@
-export const apiUrl = 'https://nbamanager.herokuapp.com'
-// export const apiUrl = "http://localhost:8080";
+let apiUrl
+
+if (process.env.NODE_ENV === 'development') {
+  apiUrl = 'http://localhost:8080'
+} else {
+  apiUrl = 'https://nbamanager.herokuapp.com'
+}
+
+export { apiUrl }
