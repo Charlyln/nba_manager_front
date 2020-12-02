cc3a5dc12ee8a4edb96f889b6ce0076e96b64382
Charlyln <charleshenry.lenue>
2020-12-03 00:35:17 +0100

    add fake data, and change chart options

--- src/components/season/charts/Charts.jsx
+++ src/components/season/charts/Charts.jsx
@@ -10,6 +10,7 @@ function Charts() {
   const [UserUuid] = useState(window.localStorage.getItem('uuid'))
   const [isLoading, setIsLoading] = useState(true)
   const [series, setSeries] = useState([])
+  const [series2, setSeries2] = useState([])
   const [options] = useState({
     chart: {
       height: 350,
@@ -29,9 +30,9 @@ function Charts() {
         show: false
       }
     },
-    colors: ['#77B6EA', '#545454'],
+    colors: ['#2E93fA', '#66DA26', '#1D1D1D', '#E91E63', '#FF9800'],
     dataLabels: {
-      enabled: true
+      // enabled: true
     },
     stroke: {
       curve: 'straight'
@@ -41,14 +42,10 @@ function Charts() {
       align: 'left'
     },
     grid: {
-      borderColor: '#e7e7e7',
       row: {
         colors: ['#f3f3f3', 'transparent'],
         opacity: 0.5
       }
-    },
-    markers: {
-      size: 1
     },
     xaxis: {
       categories: ['2020', '2021', '2022', '2023', '2024'],
@@ -61,14 +58,66 @@ function Charts() {
         text: 'Value'
       },
       min: 75,
-      max: 100
+      max: 99
     },
     legend: {
       position: 'top',
-      horizontalAlign: 'right',
-      floating: true,
-      offsetY: -25,
-      offsetX: -5
+      horizontalAlign: 'right'
+    }
+  })
+
+  const [options2] = useState({
+    chart: {
+      height: 350,
+      type: 'line',
+      dropShadow: {
+        enabled: true,
+        color: '#000',
+        top: 18,
+        left: 7,
+        blur: 10,
+        opacity: 0.2
+      },
+      zoom: {
+        enabled: false
+      },
+      toolbar: {
+        show: false
+      }
+    },
+    colors: ['#2E93fA', '#66DA26', '#1D1D1D', '#E91E63', '#FF9800'],
+    dataLabels: {
+      // enabled: true
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    title: {
+      text: 'Scoring',
+      align: 'left'
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'],
+        opacity: 0.5
+      }
+    },
+    xaxis: {
+      categories: ['2020', '2021', '2022', '2023', '2024'],
+      title: {
+        text: 'Year'
+      }
+    },
+    yaxis: {
+      title: {
+        text: 'Value'
+      },
+      min: 75,
+      max: 99
+    },
+    legend: {
+      position: 'top',
+      horizontalAlign: 'right'
     }
   })
 
@@ -97,13 +146,55 @@ function Charts() {
               ? player.value + 1
               : player.lastName === 'Thompson'
               ? player.value - 2
-              : player.value + 1
+              : player.value + 1,
+            player.value,
+            player.lastName === 'Curry'
+              ? player.value - 2
+              : player.lastName === 'Wiggins'
+              ? player.value + 1
+              : player.lastName === 'Green'
+              ? player.value - 1
+              : player.lastName === 'Thompson'
+              ? player.value + 2
+              : player.value + 1,
+            player.value
           ]
         }
       })
 
-      console.log(array)
+      const array2 = res.data.Players.sort(function (a, b) {
+        return new Date(b.value) - new Date(a.value)
+      }).map((player) => {
+        return {
+          name: player.firstName + ' ' + player.lastName,
+          data: [
+            player.value,
+            player.lastName === 'Curry'
+              ? player.value - 2
+              : player.lastName === 'Wiggins'
+              ? player.value + 1
+              : player.lastName === 'Green'
+              ? player.value - 1
+              : player.lastName === 'Thompson'
+              ? player.value + 2
+              : player.value + 1,
+            player.value,
+            player.lastName === 'Curry'
+              ? player.value - 4
+              : player.lastName === 'Wiggins'
+              ? player.value + 0
+              : player.lastName === 'Green'
+              ? player.value + 3
+              : player.lastName === 'Thompson'
+              ? player.value + 4
+              : player.value + 4,
+            player.value
+          ]
+        }
+      })
+
       setSeries(array)
+      setSeries2(array2)
       setIsLoading(false)
     } catch (error) {
       console.log(error)
@@ -127,12 +218,26 @@ function Charts() {
               justifyContent: 'center'
             }}
           >
-            <Paper elevation={10}>
+            <Paper elevation={10} style={{ margin: '5px' }}>
               <Chart
                 options={options}
                 series={series}
                 type="line"
-                width={700}
+                width={500}
+                height={500}
+                style={{
+                  backgroundColor: '#8a8a8a',
+                  color: 'black',
+                  borderRadius: '4px'
+                }}
+              />
+            </Paper>
+            <Paper elevation={10} style={{ margin: '5px' }}>
+              <Chart
+                options={options2}
+                series={series2}
+                type="line"
+                width={500}
                 height={500}
                 style={{
                   backgroundColor: '#8a8a8a',
