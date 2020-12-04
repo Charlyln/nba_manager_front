e608e7ea61310bd3ade4760eeec41653d647e7cc
Charlyln <charleshenry.lenue>
2020-12-04 18:01:04 +0100

    remove import

--- src/components/season/myteam/FreePlayer.jsx
+++ src/components/season/myteam/FreePlayer.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { IconButton } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
