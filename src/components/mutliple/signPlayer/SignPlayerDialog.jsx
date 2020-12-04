f941c34f860dc9d2ef2860422e6c4eea9a44d497
Charlyln <charleshenry.lenue>
2020-12-04 17:01:33 +0100

    resolve conditions, and put chips for feedback

--- src/components/mutliple/signPlayer/SignPlayerDialog.jsx
+++ src/components/mutliple/signPlayer/SignPlayerDialog.jsx
@@ -1,12 +1,18 @@
 import {
   Button,
+  Chip,
   Dialog,
   DialogActions,
   DialogContent,
-  DialogContentText
+  DialogTitle,
+  List,
+  ListItem,
+  ListSubheader
 } from '@material-ui/core'
 import React, { useState } from 'react'
 import updateSalaryCapLeft from '../../api calls/updateSalaryCapLeft'
+import CancelIcon from '@material-ui/icons/Cancel'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
 
 function SignPlayerDialog({
   signContract,
@@ -22,23 +28,61 @@ function SignPlayerDialog({
   TeamUuid
 }) {
   const [open, setOpen] = useState(false)
+  const [playerMood, setPlayerMood] = useState('')
+  const [tooLowMoney, setTooLowMoney] = useState(false)
 
   const proposeContract = () => {
-    if (
-      (player.TeamUuid == TeamUuid && player.contractLeft && interest > 85) ||
-      (salary < 2000000 && interest > 85)
-    ) {
-      // case where no salary cap limit
-      signContract()
-    } else if (myteamData.salaryCapLeft >= salary && interest > 85) {
-      signContract()
+    if (interest <= 85) {
+      if (interest >= 40) {
+        setPlayerMood('declined')
+      } else {
+        setPlayerMood('offended')
+      }
+      if (
+        (player.TeamUuid === TeamUuid &&
+          !player.contractLeft &&
+          myteamData.salaryCapLeft < salary) ||
+        (salary >= 2000000 && myteamData.salaryCapLeft < salary)
+      ) {
+        setTooLowMoney(true)
+      }
+    } else {
+      if (interest >= 95) {
+        setPlayerMood('joy')
+      } else {
+        setPlayerMood('ok')
+      }
+      if (player.TeamUuid === TeamUuid) {
+        if (player.contractLeft) {
+          signContract()
+        } else {
+          if (myteamData.salaryCapLeft >= salary) {
+            signContract()
+          } else {
+            setTooLowMoney(true)
+          }
+        }
+      } else {
+        if (salary < 2000000) {
+          signContract()
+        } else {
+          if (myteamData.salaryCapLeft >= salary) {
+            signContract()
+          } else {
+            setTooLowMoney(true)
+          }
+        }
+      }
     }
+
     setOpen(true)
   }
 
   const handleClose = async () => {
     await updateSalaryCapLeft(UserUuid, TeamUuid)
     setOpen(false)
+    setPlayerMood('')
+    setTooLowMoney(false)
     handleCloseAll()
     if (getMyTeam) {
       await getMyTeam()
@@ -60,30 +104,83 @@ function SignPlayerDialog({
         aria-describedby="alert-dialog-description"
       >
         <DialogContent>
-          {!player.contractLeft &&
-          myteamData.salaryCapLeft < salary &&
-          !hasSign ? (
-            <DialogContentText id="alert-dialog-description">
-              {`You don't have enough money.`}
-            </DialogContentText>
-          ) : hasSign && interest >= 95 ? (
-            <DialogContentText id="alert-dialog-description">
-              {`${player.firstName} ${player.lastName} has accepted your offer with joy !`}
-            </DialogContentText>
-          ) : hasSign && interest < 95 ? (
-            <DialogContentText id="alert-dialog-description">
-              {`${player.firstName} ${player.lastName} has accepted your offer.`}
-            </DialogContentText>
-          ) : !hasSign && interest >= 40 ? (
-            <DialogContentText id="alert-dialog-description">
-              {`${player.firstName} ${player.lastName} has declined your offer.`}
-            </DialogContentText>
-          ) : !hasSign && interest < 40 ? (
-            <DialogContentText id="alert-dialog-description">
-              {`${player.firstName} ${player.lastName} is offended by your offer !`}
-            </DialogContentText>
+          <DialogTitle>Info</DialogTitle>
+          {!hasSign ? (
+            <>
+              <List>
+                <ListSubheader component="div">
+                  {`The contract could not be completed. Here's why: `}
+                </ListSubheader>
+                {playerMood === 'declined' ? (
+                  <ListItem>
+                    <Chip
+                      style={{
+                        backgroundColor: 'rgb(217, 48, 33)'
+                      }}
+                      icon={<CancelIcon />}
+                      label={`${player.firstName} ${player.lastName} has declined your offer.`}
+                    />
+                  </ListItem>
+                ) : (
+                  ''
+                )}
+                {playerMood === 'offended' ? (
+                  <ListItem>
+                    <Chip
+                      style={{
+                        backgroundColor: 'rgb(217, 48, 33)'
+                      }}
+                      icon={<CancelIcon />}
+                      label={`${player.firstName} ${player.lastName} is offended by your offer.`}
+                    />
+                  </ListItem>
+                ) : (
+                  ''
+                )}
+                {tooLowMoney ? (
+                  <ListItem>
+                    <Chip
+                      style={{
+                        backgroundColor: 'rgb(217, 48, 33)'
+                      }}
+                      icon={<CancelIcon />}
+                      label={`You don't have enough money.`}
+                    />
+                  </ListItem>
+                ) : (
+                  ''
+                )}
+              </List>
+            </>
           ) : (
-            ''
+            <>
+              <ListSubheader component="div">
+                {`The contract has succeed !`}
+              </ListSubheader>
+              {playerMood === 'joy' ? (
+                <ListItem>
+                  <Chip
+                    style={{
+                      backgroundColor: 'rgb(76, 175, 80)'
+                    }}
+                    icon={<CheckCircleIcon />}
+                    label={`${player.firstName} ${player.lastName} has accepted your offer with joy !`}
+                  />
+                </ListItem>
+              ) : playerMood === 'ok' ? (
+                <ListItem>
+                  <Chip
+                    style={{
+                      backgroundColor: 'rgb(76, 175, 80)'
+                    }}
+                    icon={<CheckCircleIcon />}
+                    label={`${player.firstName} ${player.lastName} has accepted your offer.`}
+                  />
+                </ListItem>
+              ) : (
+                ''
+              )}
+            </>
           )}
         </DialogContent>
         <DialogActions>
