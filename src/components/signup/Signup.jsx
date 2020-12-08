ab50b2157ae0fa0bfb751c57175e2a49ee1af7f0
Charlyln <charleshenry.lenue>
2020-12-08 02:48:50 +0100

    add form onsumbibt in signup

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -33,9 +33,10 @@ function SignUp() {
     window.localStorage.removeItem('SeasonUuid')
     window.localStorage.removeItem('trainingLeft')
     window.localStorage.removeItem('canPlay')
-    setPostLoading(true)
+
     try {
       if (pseudo) {
+        setPostLoading(true)
         const res = await Axios.post(`${apiUrl}/users`, {
           pseudo
         })
@@ -69,74 +70,76 @@ function SignUp() {
 
   return (
     <>
-      <AppBar className="appBar">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            component="h6"
-            style={{ marginLeft: '30px', fontSize: 'medium' }}
-          >
-            CHOOSE YOUR PSEUDO
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Grid container>
-        <Grid item xs={12} style={{ marginTop: '250px' }}>
-          <Grid container alignItems="center" justify="center">
-            <TextField
-              style={{ margin: '20px' }}
-              id="message"
-              label="Pseudo"
-              variant="outlined"
-              autoFocus="autofocus"
-              onChange={(e) => setPseudo(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12} style={{ marginTop: '50px' }}>
+      <form onSubmit={SignupPost}>
+        <AppBar className="appBar">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              component="h6"
+              style={{ marginLeft: '30px', fontSize: 'medium' }}
+            >
+              CHOOSE YOUR PSEUDO
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Grid container>
+          <Grid item xs={12} style={{ marginTop: '250px' }}>
             <Grid container alignItems="center" justify="center">
-              {postLoading ? (
-                <Button
-                  style={{
-                    width: '85px',
-                    height: '35px'
-                  }}
-                  variant="contained"
-                  color="primary"
-                  disabled={postLoading}
-                >
-                  <CircularProgress size={23} />
-                </Button>
-              ) : postSuccess ? (
-                <Button
-                  style={{
-                    width: '85px',
-                    height: '35px'
-                  }}
-                  variant="contained"
-                  endIcon={<CheckIcon />}
-                  color="secondary"
-                >
-                  Done
-                </Button>
-              ) : (
-                <Button
-                  type="submit"
-                  style={{
-                    width: '85px',
-                    height: '35px'
-                  }}
-                  onClick={SignupPost}
-                  variant="contained"
-                  color="primary"
-                  disabled={postLoading}
-                >
-                  Signup
-                </Button>
-              )}
+              <TextField
+                required
+                style={{ margin: '20px' }}
+                id="message"
+                label="Pseudo"
+                variant="outlined"
+                autoFocus="autofocus"
+                onChange={(e) => setPseudo(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} style={{ marginTop: '50px' }}>
+              <Grid container alignItems="center" justify="center">
+                {postLoading ? (
+                  <Button
+                    style={{
+                      width: '85px',
+                      height: '35px'
+                    }}
+                    variant="contained"
+                    color="primary"
+                    disabled={postLoading}
+                  >
+                    <CircularProgress size={23} />
+                  </Button>
+                ) : postSuccess ? (
+                  <Button
+                    style={{
+                      width: '85px',
+                      height: '35px'
+                    }}
+                    variant="contained"
+                    endIcon={<CheckIcon />}
+                    color="secondary"
+                  >
+                    Done
+                  </Button>
+                ) : (
+                  <Button
+                    style={{
+                      width: '85px',
+                      height: '35px'
+                    }}
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    disabled={postLoading}
+                  >
+                    Signup
+                  </Button>
+                )}
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </form>
     </>
   )
 }
