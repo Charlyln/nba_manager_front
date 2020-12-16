72e3a508907f81bcc38e651ead35c0ad56db4a22
Charlyln <charleshenry.lenue>
2020-12-16 17:31:22 +0100

    remove useless actions and add setHasViewed

--- src/actions/tutorialActions.js
+++ src/actions/tutorialActions.js
@@ -5,27 +5,9 @@ const setTutorial = (tutorialState) => {
   }
 }
 
-const setOff = () => {
-  return {
-    type: 'SETOFF'
-  }
-}
-
-const setOn = () => {
-  return {
-    type: 'SETON'
-  }
-}
-
 const start = () => {
   return {
     type: 'START'
-  }
-}
-
-const increment = () => {
-  return {
-    type: 'INCREMENT'
   }
 }
 
@@ -59,15 +41,19 @@ const setGeneralStepZero = () => {
   }
 }
 
+const setHasViewed = () => {
+  return {
+    type: 'HASVIEWED'
+  }
+}
+
 export default {
   setTutorial,
-  setOff,
-  setOn,
-  increment,
   start,
   setGeneralTutoOn,
   setGeneralTutoOff,
   incrementGeneral,
   setGeneralStepZero,
-  decrementGeneral
+  decrementGeneral,
+  setHasViewed
 }
