ebc29832e9a01e6dbd0a42ee1635b45942584d82
Charlyln <charleshenry.lenue>
2021-01-29 13:46:08 +0100

    add token in getSeason rewards

--- src/components/api calls/getSeasonRewards.js
+++ src/components/api calls/getSeasonRewards.js
@@ -1,9 +1,14 @@
 const { default: Axios } = require('axios')
 const { apiUrl } = require('../../apiUrl')
 
-const getSeasonRewards = async (UserUuid, SeasonUuid) => {
+const getSeasonRewards = async (UserUuid, SeasonUuid, token) => {
   const res = await Axios.get(
-    `${apiUrl}/players/bestPlayers/${UserUuid}/${SeasonUuid}`
+    `${apiUrl}/players/bestPlayers/${UserUuid}/${SeasonUuid}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
   )
   return res
 }
